9df5392df441075b295353b7e6c72cf0a0249b75
olshhh <90473115+olshhh@users.noreply.github.com>
2021-09-17 16:56:00 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,9 +3,86 @@
 #include <math.h>
 #include <txlib.h>
 
+const int Inf = -1;
+
+int SolveLinear(double a, double b, double *root)
+{
+    if (a == 0)
+    {
+        return Inf;
+    }
+    else
+    {
+        *root = -b / a;
+
+        return 1;
+    }
+}
+int SolveQuadratic(double a, double b, double c, double *root1, double *root2)
+{
+    if (a == 0)
+    {
+        int RN = SolveLinear(b, c, root1);
+        *root2 = *root1;
+        return RN;
+    }
+    else
+    {
+        double d = b * b - 4 * a * c;
+    if (d < 0)
+        {
+            return 0;
+        }
+        else if (d == 0)
+        {
+            *root1 = -b / (2 * a);
+
+            return 1;
+        }
+        else
+        {
+            *root1 = (-b + sqrt(d)) / (2 * a);
+            *root2 = (-b - sqrt(d)) / (2 * a);
+
+            return 2;
+        }
+    }
+
+}
 int main()
 {
     double a = 0, b = 0, c = 0;
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    double root = 0, root1 = 0, root2 = 0;
+    int RN = 0;
+    RN = SolveQuadratic(a, b, c, &root1, &root2);
+    switch(RN)
+    {
+        case Inf:
+            printf("Infinity Roots");
+            break;
+        case 0:
+            printf("No roots");
+            break;
+        case 1:
+            printf("root1: %lf, number root: %d", root1, RN);
+            break;
+        case 2:
+            printf("root1: %lf, root2: %lf, number root: %d", root1, root2, RN);
+            break;
+
+     }
+
+    return 0;
+
+}
+
+
+
+
+
+    /*double a = 0, b = 0, c = 0;
     scanf("%lf %lf %lf", &a, &b, &c);
     double d = b * b - 4 * a * c;
     double x = 0, x1 = 0, x2 = 0;
@@ -13,7 +90,7 @@ int main()
     {
         if (b == 0)
         {
-            printf("РќРµС‚ СѓСЂР°РІРЅРµРЅРёСЏ.");
+            printf("Нет уравнения.");
         }
         else
         {
@@ -25,12 +102,12 @@ int main()
     {
         if (d < 0)
         {
-            printf("РљРѕСЂРЅРµР№ РЅРµС‚.");
+            printf("Корней нет.");
         }
         else if (d == 0)
         {
-            x = -b/2*a;
-            printf("РљРѕСЂРЅРё СЃРѕРІРїР°РґР°СЋС‚. x1 = x2 = %lf", x);
+            x = -b / (2 * a);
+            printf("Корни совпадают. x1 = x2 = %lf", x);
 
         }
         else
@@ -40,6 +117,18 @@ int main()
             printf("x1 = %lf x2 = %lf", x1, x2);
         }
     }
-    return 0;
-}
+    return 0;*/
+
+
+     /*if (a == 0){
+
+    int q = SolveLinear(b, c, &root);
+    printf("root: %lf, number root: %lf", root, q);
+
+    }
+    else
+    {
+    int q = SolveQuadratic(a, b, c, &root1, &root2);
+    printf("root1: %lf, root2: %lf, number root: %lf", root1, root2, q);
+    }   */
 
